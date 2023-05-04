ffacbcb4cc202235059de74c5d70551d9bb34561
rertzer <rertzer@student.42.fr>
2023-05-04 09:12:07 +0200

    ex00 and ex01

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rertzer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/24 14:48:49 by rertzer           #+#    #+#             */
+/*   Updated: 2023/05/03 13:51:27 by rertzer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+	public:
+		ClapTrap(const std::string name);
+		ClapTrap(const ClapTrap & src);
+		~ClapTrap(void);
+		
+		ClapTrap &	operator=(ClapTrap const &rhs);
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+	private:
+		std::string	_name;
+		int			_hit_points;
+		int			_energy_points;
+		int			_attack_damage;
+};
+
+#endif
---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rertzer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 14:01:03 by rertzer           #+#    #+#             */
+/*   Updated: 2023/05/03 14:42:10 by rertzer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+class	ScavTrap: public ClapTrap
+{
+	public :
+		ScavTrap(const std::string name);
+		ScavTrap(const ScavTrap & ct);
+		~ScavTrap(void);
+
+		ScavTrap &	operator=(const ScavTrap & rhs);
+
+		void	attack(const std::string & target);
+		void	guardGate(void);
+};
+
+#endif
